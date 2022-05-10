b2f6f6e2544eca3ee3fe7bc6cde643294568c621
xt962464 <2513669400@qq.com>
2022-05-10 11:29:25 +0800

    初始化

---
+++ src/test/java/com/demo/quartz/DemoQuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.quartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoQuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
